a6e3c9102e3ddb2d930206b7cac9b65793370182
Clara Ferraz <claraferraz@proton.me>
2024-06-10 17:37:58 -0300

    feat: adiciona o componente de navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+export const Navbar = () => {
+  return (
+    <div>
+      <div>Logo</div>
+
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+
+      <div></div>
+    </div>
+  );
+};
