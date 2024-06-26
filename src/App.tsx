cf1e387627da8a1a1d8772a2153d68dd8c24264e
Clara Ferraz <claraferraz@proton.me>
2024-06-26 18:17:12 -0300

    Feat: cards display no app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Navbar } from "./components/Navbar/Navbar";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { FeatureSection } from "./components/FeatureSection/FeatureSection";
+import { ProductSection } from "./components/ProductSection/ProductSection";
 
 export function App() {
   return (
     <div>
       <Navbar />
       <Header />
+      <ProductSection />
       <FeatureSection />
       <Footer />
     </div>
