4452eb6bbf29105bfbbabac34634e2af5d859e72
Clara Ferraz <claraferraz@proton.me>
2024-06-26 15:44:51 -0300

    Feat: menu mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,53 +3,62 @@ import NavbarCart from "../../assets/navbar-cart.svg";
 import NavbarHeart from "../../assets/navbar-heart.svg";
 import NavbarProfile from "../../assets/navbar-profile.svg";
 import NavbarSearch from "../../assets/navbar-search.svg";
-import { MenuMobile } from "../MenuMobile/MenuMobile";
+import { HamburguerMenu } from "../HamburguerMenu/HamburguerMenu";
+import { useState } from "react";
 import styles from "./styles.module.css";
 
 export const Navbar = () => {
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.logo}>
-        <img src={Logo} alt="" />
-      </div>
-      <MenuMobile />
-      <ul className={styles.links}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Shop</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+  const [open, setOpen] = useState(false);
 
-      <ul className={styles.icons}>
-        <li>
-          <a href="#">
-            <img src={NavbarProfile} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src={NavbarSearch} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src={NavbarHeart} alt="" />
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <img src={NavbarCart} alt="" />
-          </a>
-        </li>
-      </ul>
+  return (
+    <div className={`${styles.wrapper} ${open ? styles.open : ""}`}>
+      <div className={styles.mobileHeader}>
+        <div className={styles.logo}>
+          <img src={Logo} alt="" />
+        </div>
+        <div className={styles.burguer}>
+          <HamburguerMenu open={open} setOpen={setOpen} />
+        </div>
+      </div>
+      <div className={`${styles.navbar} ${open ? styles.open : ""}`}>
+        <ul className={styles.links}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Shop</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+
+        <ul className={styles.icons}>
+          <li>
+            <a href="#">
+              <img src={NavbarProfile} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={NavbarSearch} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={NavbarHeart} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={NavbarCart} alt="" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
