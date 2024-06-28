cd2f65710c523901a029ba30d808c26766cf1324
Clara Ferraz <claraferraz@proton.me>
2024-06-28 20:20:50 -0300

    Feat: seleciona as ordenações

--- src/components/FilterInput/FilterInput.tsx
+++ src/components/FilterInput/FilterInput.tsx
@@ -1,11 +1,36 @@
 import styles from "./styles.module.css";
 import filterIcon from "../../assets/system-uicons_filtering.svg";
 
-export const FilterInput = () => {
+export enum OrderOptions {
+  None,
+  AlphaAsc,
+  AlphaDesc,
+  PriceAsc,
+  PriceDesc,
+}
+
+type Props = {
+  setOrder: (order: OrderOptions) => void;
+};
+
+export const FilterInput = ({ setOrder }: Props) => {
   return (
-    <button className={styles.filterBtn}>
-      <img src={filterIcon} alt="filter icon" />
-      Filter
-    </button>
+    <div>
+      <button className={styles.filterBtn}>
+        <img src={filterIcon} alt="filter icon" />
+        Filter
+      </button>
+      <div>
+        <button onClick={() => setOrder(OrderOptions.None)}>None</button>
+        <button onClick={() => setOrder(OrderOptions.AlphaAsc)}>A-Z</button>
+        <button onClick={() => setOrder(OrderOptions.AlphaDesc)}>Z-A</button>
+        <button onClick={() => setOrder(OrderOptions.PriceAsc)}>
+          Price: low to high
+        </button>
+        <button onClick={() => setOrder(OrderOptions.PriceDesc)}>
+          Price: high to low
+        </button>
+      </div>
+    </div>
   );
 };
