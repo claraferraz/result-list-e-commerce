8b8999fad0bc7866201b5e8753b6e1885f6b8afc
Clara Ferraz <clara.ferraz@compasso.com.br>
2025-04-25 15:27:42 -0300

    Feat: responsividade auth pages

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -9,10 +9,6 @@ export const AuthPage = ({ children }: Props) => {
     <section className={styles.section}>
       <div className={styles.form}>{children}</div>
       <div className={styles.image}>
-        <img
-          src="https://furniroimagesc.s3.sa-east-1.amazonaws.com/login.jpeg"
-          alt="leafs"
-        />
       </div>
     </section>
   );
